f4129fb2f26bc0a2cb159d3e422764d556a570ff
hcg0127 <kej431003@gmail.com>
2025-12-23 23:55:43 +0900

    [Feat] #1 프로젝트 기초 코드 세팅

---
+++ src/main/java/com/nova/yeoback/YeobackApplication.java
@@ -0,0 +1,13 @@
+package com.nova.yeoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nova/yeoback/YeobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.yeoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
