57f12614f695cbb946c5e5b24efb375a7f714abb
hcg0127 <kej431003@gmail.com>
2025-12-28 00:05:46 +0900

    [Mod] #1 JpaRepository 상속 추가

--- src/main/java/com/nova/yeobaek/domain/calendar/repository/CalendarRepository.java
+++ src/main/java/com/nova/yeobaek/domain/calendar/repository/CalendarRepository.java
@@ -1,4 +1,8 @@
 package com.nova.yeobaek.domain.calendar.repository;
 
-public interface CalendarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nova.yeobaek.domain.calendar.domain.Calendar;
+
+public interface CalendarRepository extends JpaRepository<Calendar, Long>, CalendarRepositoryCustom {
 }
