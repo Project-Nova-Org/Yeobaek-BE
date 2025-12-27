617bf050a6d5a2f6d5333d997a3e6fc4e0dc5270
hcg0127 <kej431003@gmail.com>
2025-12-27 14:36:36 +0900

    [Feat] #1 Security 설정 추가

---
+++ src/main/java/com/nova/yeobaek/global/auth/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.nova.yeobaek.global.auth.controller;
+
+public class AuthController {
+}
---
+++ src/main/java/com/nova/yeobaek/global/auth/dto/response/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.nova.yeobaek.global.auth.dto.response;
+
+public class ResponseDTO {
+}
