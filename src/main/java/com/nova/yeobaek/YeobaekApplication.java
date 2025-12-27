eae7d37d13f4e4e3378bfd29bf621d5322eae3c6
hcg0127 <kej431003@gmail.com>
2025-12-27 14:48:36 +0900

    [Add] #1 TimeZone 설정(Asia/Seoul)

--- src/main/java/com/nova/yeobaek/YeobaekApplication.java
+++ src/main/java/com/nova/yeobaek/YeobaekApplication.java
@@ -1,10 +1,23 @@
 package com.nova.yeobaek;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
 public class YeobaekApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(YeobaekApplication.class, args);
