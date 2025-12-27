b71d156dd25cc9ded654b9ba2296fff51ca4e025
hcg0127 <kej431003@gmail.com>
2025-12-27 17:43:52 +0900

    [Feat] #1 도메인별 ErrorStatus 추가

---
+++ src/main/java/com/nova/yeobaek/global/error/status/CalendarErrorStatus.java
@@ -0,0 +1,17 @@
+package com.nova.yeobaek.global.error.status;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CalendarErrorStatus implements ErrorReason {
+
+	;
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+}
