339c2b00043ac336315ab13f5ac94014b99f4853
hcg0127 <kej431003@gmail.com>
2025-12-27 15:34:40 +0900

    [Feat] #1 WebClient 설정 추가

---
+++ src/main/java/com/nova/yeobaek/global/config/WebClientConfig.java
@@ -0,0 +1,35 @@
+package com.nova.yeobaek.global.config;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class WebClientConfig {
+
+	@Bean
+	public WebClient webClient() {
+		HttpClient httpClient = HttpClient.create()
+			.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+			.responseTimeout(Duration.ofSeconds(5))
+			.doOnConnected(conn ->
+				conn.addHandlerLast(new ReadTimeoutHandler(5))
+					.addHandlerLast(new WriteTimeoutHandler(5)));
+
+		return WebClient.builder()
+			.clientConnector(new ReactorClientHttpConnector(httpClient))
+			.codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024))
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.build();
+	}
+}
