2ee489d638d06a57b61cbb1940e28f5c37155d06
kimjihoon <jihungim500@gmail.com>
2026-01-02 22:23:21 +0900

    [Feat] #4 닉네임 설정 제약 DTO 추가

--- src/main/java/com/nova/yeobaek/global/auth/dto/request/RequestDTO.java
+++ src/main/java/com/nova/yeobaek/global/auth/dto/request/RequestDTO.java
@@ -1,4 +1,18 @@
 package com.nova.yeobaek.global.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class RequestDTO {
+
+    public record SignupRequest(
+        @NotBlank
+        @Size(min = 1, max = 10)
+        @Pattern(
+                regexp = "^[a-zA-Z0-9가-힣]+$",
+                message = "닉네임에는 한글, 영문, 숫자만 사용할 수 있습니다."
+        )
+        String nickname
+    ){}
 }
