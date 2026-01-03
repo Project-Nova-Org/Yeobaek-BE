90a93ad2ce7d5c4d63009d44200af6f8d7e21583
jihoonkim501 <jihungim500@gmail.com>
2026-01-03 18:38:20 +0900

    [Chore] #4 AuthErrorStatus 오타수정

--- src/main/java/com/nova/yeobaek/global/auth/exception/code/AuthErrorStatus.java
+++ src/main/java/com/nova/yeobaek/global/auth/exception/code/AuthErrorStatus.java
@@ -12,7 +12,7 @@ public enum AuthErrorStatus implements ErrorReason {
     NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4041","사용자를 찾을 수 없습니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4042","리프레시토큰을 찾을 수 없습니다."),
     REFRESH_TOKEN_MISMATCH(HttpStatus.CONFLICT,"AUTH_4091","저장된 리프레시토큰과 다릅니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4003", "유효하지 않은 리프레쉬토큰입니다.");
+    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4003", "유효하지 않은 리프레시토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
