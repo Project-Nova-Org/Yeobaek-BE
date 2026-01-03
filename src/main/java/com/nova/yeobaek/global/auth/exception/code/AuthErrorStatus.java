f170a5f945862970e1999e936210e6bac60007f8
jihoonkim501 <jihungim500@gmail.com>
2026-01-03 18:39:02 +0900

    [Chore] #4 코드충돌 해결

--- src/main/java/com/nova/yeobaek/global/auth/exception/code/AuthErrorStatus.java
+++ src/main/java/com/nova/yeobaek/global/auth/exception/code/AuthErrorStatus.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuthErrorStatus implements ErrorReason {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4041","사용자를 찾을 수 없습니다."),
-    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4042","리프레시토큰을 찾을 수 없습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4042","사용자를 찾을 수 없습니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4043","리프레시토큰을 찾을 수 없습니다."),
     REFRESH_TOKEN_MISMATCH(HttpStatus.CONFLICT,"AUTH_4091","저장된 리프레시토큰과 다릅니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4003", "유효하지 않은 리프레시토큰입니다.");
 
