5fbd6005b85248a64e086b46f5c9f1841b08561f
jihoonkim501 <jihungim500@gmail.com>
2026-01-05 10:44:16 +0900

    [Fix] #4 잘못된 import 문 수정

--- src/main/java/com/nova/yeobaek/global/auth/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/nova/yeobaek/global/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -3,11 +3,11 @@ package com.nova.yeobaek.global.auth.jwt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nova.yeobaek.global.payload.response.CommonResponse;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
