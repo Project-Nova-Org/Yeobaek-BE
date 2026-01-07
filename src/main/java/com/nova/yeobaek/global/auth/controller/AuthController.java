8adfddfc17e9893d07a117cb7dca52387b529c0e
jihoonkim501 <jihungim500@gmail.com>
2026-01-07 14:33:11 +0900

    [Fix] #8 AuthController 수정

--- src/main/java/com/nova/yeobaek/global/auth/controller/AuthController.java
+++ src/main/java/com/nova/yeobaek/global/auth/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.nova.yeobaek.global.auth.controller;
 
 import com.nova.yeobaek.domain.user.domain.User;
+import com.nova.yeobaek.domain.user.domain.enums.OauthProvider;
+import com.nova.yeobaek.domain.user.domain.enums.Role;
+import com.nova.yeobaek.domain.user.repository.UserRepository;
 import com.nova.yeobaek.domain.user.service.UserService;
 import com.nova.yeobaek.global.auth.dto.request.RequestDTO;
+import com.nova.yeobaek.global.auth.dto.response.ResponseDTO;
 import com.nova.yeobaek.global.payload.response.CommonResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,44 +24,60 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/auth")
 public class AuthController implements AuthControllerDocs {
 
-	private final AuthService authService;
-	private final UserService userService;
+    private final AuthService authService;
+    private final UserService userService;
 
-	// 구글 로그인
-	@Override
-	@GetMapping("/login/google")
-	public void googleLoginInfo(){}
+    // 소셜로그인
+    @Override
+    @PostMapping("/social/login")
+    public CommonResponse<?> socialLogin(
+            @RequestBody @Valid RequestDTO.SocialLoginRequest request
+    ) {
+        return CommonResponse.onSuccess(authService.socialLogin(request));
+    }
 
-	// 카카오 로그인
-	@Override
-	@GetMapping("/login/kakao")
-	public void kakaoLoginInfo(){}
+    // 개발용 로그인 삭제 예정
+    @Override
+    @PostMapping("/api/auth/dev-login")
+    public CommonResponse<ResponseDTO.LoginResponse> devLogin(
+            @RequestParam OauthProvider provider,
+            @RequestParam String oauthId
+    ) {
+        return CommonResponse.onSuccess(authService.devLogin(provider, oauthId));
+    }
 
-	// 닉네임 설정
-	@Override
-	@PatchMapping("/signup")
-	public CommonResponse<?> signup(
-			@AuthenticationPrincipal(expression = "user") User user,
-			@RequestBody @Valid RequestDTO.SignupRequest nickname
-	) {
-		Long userId = user.getId();
-		userService.setNickname(userId,nickname);
-		return CommonResponse.onSuccess(nickname);
-	}
+    // 닉네임 설정
+    @Override
+    @PatchMapping("/signup")
+    public CommonResponse<?> signup(
+            @AuthenticationPrincipal(expression = "user") User user,
+            @RequestBody @Valid RequestDTO.SignupRequest nickname
+    ) {
+        Long userId = user.getId();
+        userService.setNickname(userId, nickname);
+        return CommonResponse.onSuccess(nickname);
+    }
 
-	// 로그아웃
-	@Override
-	@PostMapping("/logout")
-	public CommonResponse<Void> logout(HttpServletRequest request, HttpServletResponse response) {
-		authService.logout(request,response);
-		return CommonResponse.onSuccess(null);
-	}
+    // 로그아웃
+    @Override
+    @PostMapping("/logout")
+    public CommonResponse<Void> logout(
+            @RequestHeader("Authorization") String authorization,
+            @AuthenticationPrincipal(expression = "user") User user
+    ) {
+        String accessToken = authorization.replace("Bearer ", "");
+        authService.logout(accessToken, user.getId());
+        return CommonResponse.onSuccess(null);
+    }
 
-	// AccessToken 재발급
-	@Override
-	@PostMapping("/reissue")
-	public CommonResponse<?> reissue(HttpServletRequest request, HttpServletResponse response){
-		authService.reissue(request,response);
-		return CommonResponse.onSuccess(null);
-	}
+    // AccessToken 재발급
+    @Override
+    @PostMapping("/reissue")
+    public CommonResponse<?> reissue(
+            @RequestBody @Valid RequestDTO.ReissueRequest request
+    ) {
+        return CommonResponse.onSuccess(
+                authService.reissue(request.refreshToken())
+        );
+    }
 }
