73f466b6dad3c6a0e40e71d8b9868cd9741c0eec
jihoonkim501 <jihungim500@gmail.com>
2026-01-07 15:09:54 +0900

    [Fix] #8 codeRabbit 리뷰 중복된 에러코드 변경

--- src/main/java/com/nova/yeobaek/global/auth/exception/code/AuthErrorStatus.java
+++ src/main/java/com/nova/yeobaek/global/auth/exception/code/AuthErrorStatus.java
@@ -13,11 +13,11 @@ public enum AuthErrorStatus implements ErrorReason {
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4043","리프레시토큰을 찾을 수 없습니다."),
     REFRESH_TOKEN_MISMATCH(HttpStatus.CONFLICT,"AUTH_4091","저장된 리프레시토큰과 다릅니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4003", "유효하지 않은 액세스토큰입니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4003", "유효하지 않은 리프레시토큰입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4004", "유효하지 않은 리프레시토큰입니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_4012", "토큰이 만료되었습니다."),
     BLACKLISTED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_4013", "이미 로그아웃된 토큰입니다."),
-    INVALID_PROVIDER(HttpStatus.BAD_REQUEST,"AUTH_4004","유효하지 않은 소셜 로그인 타입입니다."),
-    INVALID_OAUTH_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4005","유효하지 않은 소셜 로그인 토큰입니다.");
+    INVALID_PROVIDER(HttpStatus.BAD_REQUEST,"AUTH_4005","유효하지 않은 소셜 로그인 타입입니다."),
+    INVALID_OAUTH_TOKEN(HttpStatus.BAD_REQUEST,"AUTH_4006","유효하지 않은 소셜 로그인 토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
