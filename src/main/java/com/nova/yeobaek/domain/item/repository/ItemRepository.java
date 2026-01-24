3849cda4f0d818bbb36824e1076d6f8513350b36
chanudevelop <chanu.develop@gmail.com>
2026-01-24 17:39:08 +0900

    [Feat] #30 아이템 수정,삭제 Repository

--- src/main/java/com/nova/yeobaek/domain/item/repository/ItemRepository.java
+++ src/main/java/com/nova/yeobaek/domain/item/repository/ItemRepository.java
@@ -1,8 +1,13 @@
 package com.nova.yeobaek.domain.item.repository;
 
+import com.nova.yeobaek.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nova.yeobaek.domain.item.domain.Item;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
+
+    Optional<Item> findByIdAndUser(Long id, User user);
 }
--- src/main/java/com/nova/yeobaek/domain/ootd/repository/OOTDRepository.java
+++ src/main/java/com/nova/yeobaek/domain/ootd/repository/OOTDRepository.java
@@ -1,8 +1,17 @@
 package com.nova.yeobaek.domain.ootd.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.nova.yeobaek.domain.ootd.domain.OOTD;
+import com.nova.yeobaek.domain.ootd.domain.enums.OOTDStatus;
 
 public interface OOTDRepository extends JpaRepository<OOTD, Long>, OOTDRepositoryCustom {
+
+    @Modifying
+    @Query("UPDATE OOTD o SET o.status = :status WHERE o.id IN " +
+            "(SELECT oi.ootd.id FROM OOTDItem oi WHERE oi.item.id = :itemId)")
+    void updateStatusByItemId(@Param("itemId") Long itemId, @Param("status") OOTDStatus status);
 }
