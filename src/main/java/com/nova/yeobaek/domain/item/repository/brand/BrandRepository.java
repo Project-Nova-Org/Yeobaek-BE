36f2549cfbe47396d5fafc9c6e63c64cccebf8de
chanudevelop <chanu.develop@gmail.com>
2026-01-24 17:11:01 +0900

    [Feat] #30 아이템 등록 repository 작성

--- src/main/java/com/nova/yeobaek/domain/item/repository/brand/BrandRepository.java
+++ src/main/java/com/nova/yeobaek/domain/item/repository/brand/BrandRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nova.yeobaek.domain.item.domain.Brand;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+    Optional<Brand> findByName(String name);
 }
