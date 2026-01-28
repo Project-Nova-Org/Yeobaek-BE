3397d4ed4099afdc0908277a4ad7d2365442f1b5
jihoonkim501 <jihungim500@gmail.com>
2026-01-28 13:18:13 +0900

    [Feat] #7 맞춤정보, 마이페이지 DTO 구현

--- src/main/java/com/nova/yeobaek/domain/user/dto/response/UserResponseDTO.java
+++ src/main/java/com/nova/yeobaek/domain/user/dto/response/UserResponseDTO.java
@@ -1,4 +1,47 @@
 package com.nova.yeobaek.domain.user.dto.response;
 
+import com.nova.yeobaek.domain.user.domain.User;
+import com.nova.yeobaek.domain.user.domain.enums.Gender;
+import com.nova.yeobaek.domain.user.domain.enums.OauthProvider;
+import com.nova.yeobaek.domain.user.domain.enums.Rank;
+
 public class UserResponseDTO {
+    public record PreferenceResponse(
+            float height,
+            float weight,
+            Gender gender,
+            String bodyImageUrl
+    ) {
+        public static PreferenceResponse from(User user) {
+            return new PreferenceResponse(
+                    user.getHeight(),
+                    user.getWeight(),
+                    user.getGender(),
+                    user.getBodyImageUrl()
+            );
+        }
+    }
+
+    public record GetMyPageResponse(
+
+            Long userId,
+            String nickname,
+            String profileImageUrl,
+            String email,
+            Rank rank,
+            String rankName,
+            OauthProvider oauthProvider
+    ) {
+        public static GetMyPageResponse from(User user) {
+            return new GetMyPageResponse(
+                    user.getId(),
+                    user.getNickname(),
+                    user.getEmail(),
+                    user.getProfileImageUrl(),
+                    user.getRank(),
+                    user.getRank().getName(),
+                    user.getOauthProvider()
+            );
+        }
+    }
 }
