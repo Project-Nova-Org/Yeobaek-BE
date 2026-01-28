370c3406be7863c5ca750f3ea79f4ac53d522d5c
jihoonkim501 <jihungim500@gmail.com>
2026-01-28 13:15:36 +0900

    [Feat] #7 유저관련 Error status 추가

--- src/main/java/com/nova/yeobaek/domain/user/status/UserErrorStatus.java
+++ src/main/java/com/nova/yeobaek/domain/user/status/UserErrorStatus.java
@@ -13,7 +13,10 @@ public enum UserErrorStatus implements ErrorReason {
 
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND,"AUTH_4041","사용자를 찾을 수 없습니다."),
 	DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST,"AUTH_4002","중복된 닉네임을 선택했습니다."),
-	ALREADY_SIGNUP(HttpStatus.BAD_REQUEST,"AUTH_4001","이미 닉네임을 설정한 유저입니다.");
+	SAME_NICK_NAME(HttpStatus.BAD_REQUEST,"AUTH_4001","변경 전 닉네임입니다."),
+	ALREADY_SIGNUP(HttpStatus.BAD_REQUEST,"AUTH_4003","이미 닉네임을 설정한 유저입니다."),
+	INVALID_PREFERENCE(HttpStatus.BAD_REQUEST,"AUTH_4004","잘못된 맞춤정보 요청입니다."),
+	ALREADY_WITHDRAWN(HttpStatus.BAD_REQUEST,"AUTH_4005","이미 탈퇴한 유저입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
