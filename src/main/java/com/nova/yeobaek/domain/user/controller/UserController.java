3ae08789fa4190e56a357a01ecfd13929fb31b4e
jihoonkim501 <jihungim500@gmail.com>
2026-01-28 13:18:45 +0900

    [Feat] #7 유저관련 API controller / docs 추가

--- src/main/java/com/nova/yeobaek/domain/user/controller/UserController.java
+++ src/main/java/com/nova/yeobaek/domain/user/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.nova.yeobaek.domain.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nova.yeobaek.domain.user.domain.User;
+import com.nova.yeobaek.domain.user.dto.request.UserRequestDTO;
+import com.nova.yeobaek.domain.user.dto.response.UserResponseDTO;
+import com.nova.yeobaek.global.auth.dto.request.RequestDTO;
+import com.nova.yeobaek.global.auth.security.CustomUserDetails;
+import com.nova.yeobaek.global.payload.response.CommonResponse;
+import jakarta.validation.Valid;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import com.nova.yeobaek.domain.user.controller.docs.UserControllerDocs;
 import com.nova.yeobaek.domain.user.service.UserService;
@@ -12,8 +19,51 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/api/users/me")
 public class UserController implements UserControllerDocs {
 
-	private final UserService userService;
+    private final UserService userService;
+
+    @Override
+    @PatchMapping("/nickname")
+    public CommonResponse<?> setNickname(
+            @AuthenticationPrincipal(expression = "user") User user,
+            @RequestBody @Valid RequestDTO.SignupRequest newNickname
+    ) {
+        Long userId = user.getId();
+        userService.setNickname(userId, newNickname);
+        return CommonResponse.onSuccess(newNickname);
+    }
+
+    @Override
+    @PatchMapping("/preferences")
+    public CommonResponse<UserResponseDTO.PreferenceResponse> setPreference(
+            @AuthenticationPrincipal(expression = "user") User user,
+            @RequestBody @Valid UserRequestDTO.PreferenceRequest request
+    ) {
+        Long userId = user.getId();
+        UserResponseDTO.PreferenceResponse response =
+                userService.setPreference(userId, request);
+
+        return CommonResponse.onSuccess(response);
+    }
+
+    @Override
+    @DeleteMapping("/me")
+    public CommonResponse<Void> withdraw(
+            @AuthenticationPrincipal(expression = "user") User user
+    ) {
+        userService.withdraw(user.getId());
+        return CommonResponse.onSuccess(null);
+    }
+
+    @Override
+    @GetMapping("/me")
+    public CommonResponse<UserResponseDTO.GetMyPageResponse> getMypage(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        UserResponseDTO.GetMyPageResponse
+                response = userService.getMypage(userDetails.getUser().getId());
+        return CommonResponse.onSuccess(response);
+    }
 }
