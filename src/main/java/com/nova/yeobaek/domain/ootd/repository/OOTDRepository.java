e7ecafbf4c51a08207255ee31b1cb1680fb54c98
chanudevelop <chanu.develop@gmail.com>
2026-01-30 10:04:15 +0900

    [Feat] #40 아이템이 포함된 OOTD 목록 조회 API 구현

--- src/main/java/com/nova/yeobaek/domain/ootd/repository/OOTDRepository.java
+++ src/main/java/com/nova/yeobaek/domain/ootd/repository/OOTDRepository.java
@@ -8,10 +8,20 @@ import org.springframework.data.repository.query.Param;
 import com.nova.yeobaek.domain.ootd.domain.OOTD;
 import com.nova.yeobaek.domain.ootd.domain.enums.OOTDStatus;
 
+import java.util.List;
+
 public interface OOTDRepository extends JpaRepository<OOTD, Long>, OOTDRepositoryCustom {
 
     @Modifying(clearAutomatically = true)
     @Query("UPDATE OOTD o SET o.status = :status, o.updatedAt = CURRENT_TIMESTAMP WHERE o.id IN " +
             "(SELECT oi.ootd.id FROM OOTDItem oi WHERE oi.item.id = :itemId)")
     void updateStatusByItemId(@Param("itemId") Long itemId, @Param("status") OOTDStatus status);
+
+    @Query("SELECT DISTINCT o FROM OOTD o " +
+            "JOIN o.ootdItemList oi " +
+            "WHERE oi.item.id = :itemId " +
+            "AND o.user.id = :userId " +
+            "AND o.status = 'NORMAL' " +
+            "ORDER BY o.id DESC")
+    List<OOTD> findAllByItemIdAndUserId(@Param("itemId") Long itemId, @Param("userId") Long userId);
 }
