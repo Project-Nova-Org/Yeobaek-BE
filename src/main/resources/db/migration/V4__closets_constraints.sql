ea3fb415624ad6de6803680d541771fa26923083
rkd928 <ind07316@naver.com>
2026-02-05 11:31:57 +0900

    [Fix] #37 uk_closet_user_name 존재 체크 범위 conrelid로 제한

--- src/main/resources/db/migration/V4__closets_constraints.sql
+++ src/main/resources/db/migration/V4__closets_constraints.sql
@@ -11,7 +11,10 @@ ALTER TABLE closets
 DO $$
 BEGIN
     IF NOT EXISTS (
-        SELECT 1 FROM pg_constraint WHERE conname = 'uk_closet_user_name'
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'uk_closet_user_name'
+          AND conrelid = 'closets'::regclass
     ) THEN
 ALTER TABLE closets
     ADD CONSTRAINT uk_closet_user_name UNIQUE (user_id, name);
